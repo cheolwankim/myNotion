76651468dee01f7d9e22b1ef72de2be8d2ba5065
CheolWan <sam_000@naver.com>
2025-04-24 17:59:06 +0900

    배포용 API주소 적용 및 axios 설정 변경

--- notion-frontend/lib/axios.ts
+++ notion-frontend/lib/axios.ts
@@ -1,10 +1,24 @@
+// lib/axios.ts
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ 백엔드 주소 + /api
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // ✅ .env.local에서 가져옴
+  withCredentials: true, // 필요 시 (JWT 쿠키 등)
 });
 
 export default instance;
+
+
+
+
+
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:5000/api", // ✅ 백엔드 주소 + /api  로컬에서 가능
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default instance;
